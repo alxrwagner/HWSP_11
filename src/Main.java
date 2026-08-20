a5c9c91e0dc3e183de2ca172b926dee157d9b8cd
alxrwagner <alxr.wagner@gmail.com>
2026-08-20 18:54:44 +0300

    Поставил фигурные скобки

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,9 @@ public class Main {
             System.out.println("Високосный год был введен в 1584 году");
         } else if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0) {
             System.out.println("Год является високосным");
-        } else System.out.println("Год не является високосным");
+        } else {
+            System.out.println("Год не является високосным");
+        }
     }
 
     public static void selectAppBuild(int clientOS, int yearReleaseDevice) {
@@ -42,7 +44,9 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1 && yearReleaseDevice >= currentYear) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else System.out.println("Для Вашей операционной системы приложение скоро будет доступно");
+        } else {
+            System.out.println("Для Вашей операционной системы приложение скоро будет доступно");
+        }
     }
 
     public static void calculateDeliveryDays(int deliveryDistance) {
@@ -54,6 +58,8 @@ public class Main {
             System.out.println("Потребуется дней: " + (deliveryDates + 1));
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
             System.out.println("Потребуется дней: " + (deliveryDates + 2));
-        } else System.out.println("Доставки нет");
+        } else {
+            System.out.println("Доставки нет");
+        }
     }
 }
